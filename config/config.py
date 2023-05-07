40e763e18f4b58ce5afc9b901c1f34179c445c58
Polonez1 <darjus.vasiukevic@gmail.com>
2023-05-07 23:21:35 +0300

    updated config

---
+++ config/config.py
@@ -0,0 +1,2 @@
+MATCHES_URL = "https://api.football-data-api.com/league-matches?key={api_token}&season_id={season_id}"
+TEAMS_URL = "https://api.football-data-api.com/league-teams?key={api_token}&season_id={season_id}&include=stats"
