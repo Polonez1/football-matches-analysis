ee6d9be4a247f045b06147b298eb5644dd87cf1d
Polonez1 <darjus.vasiukevic@gmail.com>
2023-05-07 23:43:16 +0300

    created connect fnc

--- scripts/modules/API_connect.py
+++ scripts/modules/API_connect.py
@@ -1,1 +1,28 @@
 import requests
+import logging
+import sys
+import json
+
+sys.path.append(".\\config\\")
+import secret
+import config
+
+config.log
+
+
+def connect_to_footystats(url: str, **kwargs):
+    url = url.format(api_token=secret.API_TOKEN, season_id=secret.SEASON_ID, **kwargs)
+    response = requests.get(url)
+    logging.info(response.status_code)
+    logging.info(response.request)
+    return response
+
+
+def get_json_API(response: requests):
+    jsonObj = response.json()
+    return jsonObj
+
+
+if __name__ == "__main__":
+    response = connect_to_footystats(url=config.TEAMS_URL)
+    js = get_json_API(response)
