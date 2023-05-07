1911d159c58b07bceaaf707c38ddfd9571c1cacb
Polonez1 <darjus.vasiukevic@gmail.com>
2023-05-07 23:26:46 +0300

    created new folders and updated config.py

--- config/config.py
+++ config/config.py
@@ -1,2 +1,9 @@
+import logging
+import sys
+
+# URL
 MATCHES_URL = "https://api.football-data-api.com/league-matches?key={api_token}&season_id={season_id}"
 TEAMS_URL = "https://api.football-data-api.com/league-teams?key={api_token}&season_id={season_id}&include=stats"
+
+# logging
+log = logging.basicConfig(stream=sys.stdout, level=logging.INFO)
